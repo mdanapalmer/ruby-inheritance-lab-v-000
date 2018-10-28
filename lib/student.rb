503e2b8c21cd0852a841de7e110a97bc07608350
Dana Palmer <roiai4@hotmail.com>
2018-10-28 19:33:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,9 @@
 class Student < User
   attr_accessor :first_name, :last_name
 
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+    @knowledge = []
+
 end
